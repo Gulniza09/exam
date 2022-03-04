795251142e2fcd6006fb802976d2c4a68fc8ae24
Gulniza <gulnizajanyshova@gmail.com>
2022-03-04 21:13:50 +0300

    exam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([])
+	console.log(data , 'get')
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => {
+				if (response.ok) {
+					return response.json()
+				}
+			})
+			.then((data) => setData(data))
+	}, [])
+
+	// ___________Post_____________
+
+	const postTracker = async (data) => {
+		const response = await fetch(
+			'https://jsonplaceholder.typicode.com/posts',
+			{
+				method: 'POST',
+				headers: { 'Content-type': 'application/json' },
+				body: JSON.stringify(data[2]),
+			},
+		)
+		const dataa = await response.json()
+    console.log(dataa , 'post');
+
+		
+	}
+
+	return (
+		<div className='App'>
+			<button onClick={postTracker}>Post</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
